b8537a1bea0dcb04d62b7db885c3b8de2ab7117f
Vicente Miranda <vicentem@google.com>
2016-11-12 19:55:56 -0800

    Add GPIO for RPI

--- routes.py
+++ routes.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO
+# import RPi.GPIO as GPIO
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
+# GPIO.setup(17, GPIO.OUT)
+# GPIO.setup(18, GPIO.OUT)
+
+action = {False: 0, True: 1}
+state = False
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -17,9 +26,11 @@ def about():
 def start():
     if request.method == "POST":
         if request.form['btn'] == 'right':
-            print 'right pressed'
+            print 'right pressed and action'
+            # GPIO.output(17, 1)
         elif request.form['btn'] == 'left':
             print 'left pressed'
+            # GPIO.output(17, 0)
         elif request.form['btn'] == 'down':
             print 'down pressed'
         elif request.form['btn'] == 'up':
@@ -32,4 +43,4 @@ def hello(name, age, weight):
     return "Hello, {} your age is {} and you weight {}".format(name, age, weight)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0')
